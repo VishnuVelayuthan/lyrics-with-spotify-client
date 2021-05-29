0bd9f6f149b682ebd071f1a57ee186fd1252b001
Vishnu Velayuthan <100023569@mvla.net>
2021-05-28 18:57:45 -0700

    Got the auth link to work

--- src/Login.js
+++ src/Login.js
@@ -7,14 +7,17 @@ const scopes = `streaming%20user-read-email%20user-read-private%20user-
 library-read%20user-library-modify%20user-read-playback-state%20
 user-modify-playback-state`;
 
-const AUTH_URL = `https://accounts.spotify.com/authorize
-    ?client_id=`+client_id+`&response_type=code&redirect_uri=`+redirect_uri+`
-    &scope=`+scopes;
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id="+client_id+
+    "&response_type=code&redirect_uri="+redirect_uri+
+    "&scope="+scopes;
 
 export default function Login() {
 
     return (
-        <Container>
+        <Container 
+            className="d-flex justify-content-center align-items-center"
+            style={{minHeight : "100vh"}}
+        >
             <a className="btn btn-success btn-lg" href={AUTH_URL}>
                 Login with Spotify
             </a>
